1410be2fd6009c9da600daa16c60a367b7af591b
webseed87 <thdwngusl@naver.com>
2024-09-13 13:47:45 +0900

    first commit

---
+++ js/common.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.top-menu').addEventListener('mouseenter', function () {
+        document.querySelector('header').classList.add('hover');
+    });
+
+    document.querySelector('header').addEventListener('mouseleave', function () {
+        document.querySelector('header').classList.remove('hover');
+    });
+    document.querySelector('.icon-bar').addEventListener("click", function () {
+        document.querySelector('#header').classList.toggle('all-menu');
+        document.querySelector('body').classList.toggle('overflow');
+    });
+
+const partnerTabItems = document.querySelectorAll('.partner-tab li'); 
+    partnerTabItems.forEach(function(item) {
+        item.addEventListener('click', function() {
+            partnerTabItems.forEach(function(el) {
+                el.classList.remove('active');
+            });
+            this.classList.add('active');
+        });
+    });
+
+});
+
+const io = new IntersectionObserver(function (entries) {
+    entries.forEach(function (entry) {
+        if (!entry.isIntersecting) {
+            return
+        }
+        entry.target.classList.add('show')
+    });
+});
+const infoEls = document.querySelectorAll('.ani-bottom-top')
+infoEls.forEach(function (el) {
+    io.observe(el)
+});
+const infoLeftRightEls = document.querySelectorAll('.ani-left-right');
+infoLeftRightEls.forEach(function (el) {
+    io.observe(el);
+});
+const fadeInEls = document.querySelectorAll('.ani-fade-in');
+fadeInEls.forEach(function (el) {
+    io.observe(el);
+});
